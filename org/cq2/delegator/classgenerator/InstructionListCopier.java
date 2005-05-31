3414023688af4ea5d2cb160146c21964a0a9c6ce
Klaas van Schelven <klaas@vanschelven.com>
2005-05-31 13:49:39 +0000

    somewhere halfway the formidable task of copying constants from one pool to another and updating indixes (2)

--- org/cq2/delegator/classgenerator/InstructionListCopier.java
+++ org/cq2/delegator/classgenerator/InstructionListCopier.java
@@ -1,7 +1,12 @@
 package org.cq2.delegator.classgenerator;
 
+import java.util.HashMap;
+
 import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantClass;
 import org.apache.bcel.classfile.ConstantFieldref;
+import org.apache.bcel.classfile.ConstantNameAndType;
+import org.apache.bcel.classfile.ConstantUtf8;
 import org.apache.bcel.generic.CPInstruction;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.InstructionList;
@@ -11,25 +16,33 @@ public class InstructionListCopier {
     private ConstantPoolGen constantPool;
     private ConstantPoolGen originalConstantPool;
     private static final int FIRSTCONSTANTISRESERVEDBYJVM = 1;
+    private HashMap constantMap;
     
     public InstructionListCopier(ConstantPoolGen originalConstantPool, ConstantPoolGen constantPool) {
         this.originalConstantPool = originalConstantPool;
         this.constantPool = constantPool;
+        constantMap = new HashMap();
     }
     
     public InstructionList copy(InstructionList original) {
         for (int i = FIRSTCONSTANTISRESERVEDBYJVM; i < originalConstantPool.getSize(); i++) {
-            constantPool.addConstant(originalConstantPool.getConstant(i), originalConstantPool);
+            int newIndex = constantPool.addConstant(originalConstantPool.getConstant(i), originalConstantPool);
+            constantMap.put(new Integer(i), new Integer(newIndex));
         }
         InstructionList result = original.copy();
         for (int i = 0; i < result.getInstructions().length; i++) {
             if (result.getInstructions()[i] instanceof CPInstruction) {
                 CPInstruction instruction = (CPInstruction) result.getInstructions()[i];
-                Constant originalConstant = originalConstantPool.getConstant(instruction.getIndex());
-                instruction.setIndex(find(originalConstant));
+//                Constant originalConstant = originalConstantPool.getConstant(instruction.getIndex());
+//                instruction.setIndex(find(originalConstant));
+                instruction.setIndex(getNewIndex(instruction.getIndex()));
             }
         }
         return result;
+    }
+
+    private int getNewIndex(int index) {
+        return ((Integer) constantMap.get(new Integer(index))).intValue();
     }
 
     private int find(Constant originalConstant) {
@@ -48,14 +61,27 @@ public class InstructionListCopier {
 //        constantPool.lookupString("");
 //        constantPool.lookupUtf8("");
         
-//        if (Constant instanceof Fi)
-//        constantPool.look
 //        for (int i = FIRSTCONSTANTISRESERVEDBYJVM; i < constantPool.getSize(); i++) {
-//            if (constantPool.getConstant(i).equals(originalConstant))
+//            if (constantRepresentation(constantPool.getConstant(i)).equals(constantRepresentation(originalConstant)))
 //                return i;
 //        }
         throw new RuntimeException("Should not happen...");
         //return -1;
+    }
+    
+    private String constantRepresentation(Constant constant) {
+        if (constant instanceof ConstantUtf8) return ((ConstantUtf8)constant).getBytes();
+        if (constant instanceof ConstantFieldref) {
+            ConstantFieldref fieldref = (ConstantFieldref)constant;
+            return constantRepresentation(originalConstantPool.getConstant(fieldref.getClassIndex())) +
+              constantRepresentation(originalConstantPool.getConstant(fieldref.getNameAndTypeIndex()));
+        }
+        if (constant instanceof ConstantClass) 
+            return ((ConstantClass)constant).getBytes(originalConstantPool.getConstantPool());
+        if (constant instanceof ConstantNameAndType) 
+            return ((ConstantNameAndType)constant).getName(originalConstantPool.getConstantPool()) + 
+            ((ConstantNameAndType)constant).getSignature(originalConstantPool.getConstantPool());
+        throw new RuntimeException("Define more cases, specifically for " + constant);
     }
     
 }
--- org/cq2/delegator/classgenerator/InstructionListCopierTest.java
+++ org/cq2/delegator/classgenerator/InstructionListCopierTest.java
@@ -63,6 +63,4 @@ public class InstructionListCopierTest extends TestCase {
         runner.runVoid(result);
     }
     
-
-    
 }
